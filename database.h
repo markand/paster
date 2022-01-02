d3b94082e6f1eeb18a95e81f83f582932b5ad28e
David Demelier <markand@malikania.fr>
2022-01-02 10:24:20 +0100

    misc: update copyright years

--- database.h
+++ database.h
@@ -1,7 +1,7 @@
 /*
  * database.h -- sqlite storage
  *
- * Copyright (c) 2020-2021 David Demelier <markand@malikania.fr>
+ * Copyright (c) 2020-2022 David Demelier <markand@malikania.fr>
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
